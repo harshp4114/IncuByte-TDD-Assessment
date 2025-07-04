7ab1b454cbdf294b7081ea1c30b41d7ff7f49521
harshp4114 <harshpatadia4114@gmail.com>
2025-07-04 22:55:32 +0530

    [Test-2] Add failing test for input of single number

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,13 +1,21 @@
 const add = require("../src/stringCalculator");
 
+// Test Suite for Jest functionality
 describe("Jest Working Test Suite", () => {
   test("Jest is working", () => {
     expect(1).toBe(1);
   });
 });
 
+// Test Suite for String Calculator
 describe("String Calculator Test Suite", () => {
+  // Test for empty string  
   test("Empty string returns 0", () => {
     expect(add("")).toBe(0);
   });
+
+  // Test for single number addition
+  test("Single number returns the number itself",()=>{
+    expect(add("1")).toBe(1);
+  })
 });
