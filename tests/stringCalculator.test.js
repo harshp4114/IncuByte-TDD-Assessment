9e7e9aab53158002244992b8dc9f4e3fa01e0df4
harshp4114 <harshpatadia4114@gmail.com>
2025-07-04 23:02:53 +0530

    [Test-4] Add failing test for sum of multiple numbers separated by commas

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -9,18 +9,23 @@ describe("Jest Working Test Suite", () => {
 
 // Test Suite for String Calculator
 describe("String Calculator Test Suite", () => {
-  // Test for empty string  
+  // Test for empty string
   test("Empty string returns 0", () => {
     expect(add("")).toBe(0);
   });
 
   // Test for single number addition
-  test("Single number returns the number itself",()=>{
+  test("Single number returns the number itself", () => {
     expect(add("1")).toBe(1);
-  })
+  });
 
   // Test for addition of two numbers
   test("Two numbers comma separated returns their sum", () => {
     expect(add("1,5")).toBe(6);
+  });
+
+  // Test for addition of multiple numbers
+  test("Multiple numbers comma separated returns their sum", () => {
+    expect(add("1,2,3,4")).toBe(10);
   });
 });
