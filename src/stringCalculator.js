ed70f153af8257e25ee86c3a74cd49565b1f214a
harshp4114 <harshpatadia4114@gmail.com>
2025-07-05 00:49:26 +0530

    [Pass-8] Should ignore numbers greater than 1000 during addition

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -10,22 +10,25 @@ function getDelimiterAndNumbers(input) {
   return { delimiter: /[,\n]/, numbers: input };
 }
 
-function checkForNegatives(numbers){
-    const negatives = numbers.filter(num => num < 0);
-    if (negatives.length > 0) {
-        throw new Error(`negative numbers not allowed ${negatives.join(", ")}`);
-    };
+function checkForNegatives(numbers) {
+  const negatives = numbers.filter((num) => num < 0);
+  if (negatives.length > 0) {
+    throw new Error(`negative numbers not allowed ${negatives.join(", ")}`);
+  }
 }
 
 function add(numbers) {
   if (numbers === "") return 0;
 
   const { delimiter, numbers: sanitized } = getDelimiterAndNumbers(numbers);
-  const values = sanitized.split(delimiter).map((num)=>Number(num));
+  const values = sanitized.split(delimiter).map((num) => Number(num));
 
   checkForNegatives(values);
 
-  return values.reduce((sum, num) => sum + num, 0);
+  return values.reduce((sum, num) => {
+    if(num>1000) return sum+0;
+    return sum + num;
+  }, 0);
 }
 
 module.exports = add;
