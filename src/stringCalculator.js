558f6ec60f10d6170d4091bebf354e0e779bc474
harshp4114 <harshpatadia4114@gmail.com>
2025-07-05 00:22:15 +0530

    [Refactor-6] Should return sum of multiple numbers with custom delimeters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,15 +1,21 @@
-function add(numbers) {
-  if (numbers == "") return 0;
-  let delimiter = /\n|,/;
-  if (numbers.startsWith("//")) {
-    const match = numbers.match(/^\/\/(.)\n(.*)$/);
+function getDelimiterAndNumbers(input) {
+  if (input.startsWith("//")) {
+    const match = input.match(/^\/\/(.)\n(.*)$/);
     if (match) {
-      const customDelimiter = match[1];
-      delimiter = new RegExp(`[${customDelimiter}]`);
-      numbers = match[2];
+      const delimiter = new RegExp(`[${match[1]}]`);
+      const numbers = match[2];
+      return { delimiter, numbers };
     }
   }
-  const values = numbers.split(delimiter);
+  return { delimiter: /[,\n]/, numbers: input };
+}
+
+function add(numbers) {
+  if (numbers === "") return 0;
+
+  const { delimiter, numbers: sanitized } = getDelimiterAndNumbers(numbers);
+  const values = sanitized.split(delimiter);
+
   return values.reduce((sum, num) => sum + Number(num), 0);
 }
 
