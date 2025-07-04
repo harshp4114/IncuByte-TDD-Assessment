5344b5923c895f4e038f5c5b088ff801c0cba96e
harshp4114 <harshpatadia4114@gmail.com>
2025-07-04 23:05:13 +0530

    [Pass-4] Should return sum of multiple numbers separated by commas

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,12 @@
 function add(numbers){
     if(numbers=="")return 0;
     if(numbers.length==1)return Number(numbers);
-    return Number(numbers[0]) + Number(numbers[2]);
+    const parts= numbers.split(",");
+    let sum = 0;
+    for(let i=0; i<parts.length; i++){
+        sum += Number(parts[i]);
+    }
+    return sum;
 }
 
 module.exports = add;
