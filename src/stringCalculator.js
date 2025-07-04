2981575b087afd40998ca69932d54eb0f7f39616
harshp4114 <harshpatadia4114@gmail.com>
2025-07-04 23:34:43 +0530

    [Pass-5] Should return sum of multiple numbers separated by commas and newlines

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,6 +1,7 @@
 function add(numbers) {
   if (numbers == "") return 0;
-  const values = numbers.split(",");
+  const sanitized=numbers.replace(/\n/g,",");
+  const values = sanitized.split(",");
   return values.reduce((sum, num) => sum + Number(num), 0);
 }
 
